0651a921ae2123861b93915fb8e31ea0019be348
alex marcos <alex.marcos_gutierrez@roche.com>
2025-10-03 16:17:57 +0200

    remove class in headers

--- common/components/header/header.component.tsx
+++ common/components/header/header.component.tsx
@@ -22,6 +22,7 @@ export function Header() {
       stopRefresh();
     };
   }, [user]);
+
   return (
     <header className="border-b bg-background sticky top-0 z-50">
       <div className="container mx-auto px-4 py-3">
